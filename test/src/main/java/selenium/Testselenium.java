306d0117931a468443c801811d73f9f64a398fff
luesparza <luesparza@192.168.1.64>
2021-08-21 11:18:53 -0500

    first commit

---
+++ test/src/main/java/selenium/Testselenium.java
@@ -0,0 +1,13 @@
+package selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Testselenium {
+	public static void main(String[] args) {
+		String exePath = "./driver/chromedriver";
+		System.setProperty("webdriver.chrome.driver", exePath);
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.google.com");
+	}
+}
